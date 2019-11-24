682d8c6176e1a17b26b17bb39feba9ddf61110ef
Alex Zhao <oatmonster@gmail.com>
2019-11-24 01:18:18 -0800

    Added support for location and timezone based on ip

--- api/v2/index.js
+++ api/v2/index.js
@@ -1,5 +1,9 @@
 const express = require( 'express' );
 const router = express.Router();
+const fs = require( 'fs' );
+const request = require( 'request-promise-native' );
+const auth = JSON.parse( fs.readFileSync( './auth.json' ) );
+const ipApiKey = auth.ipApiKey;
 
 const search = require( './controllers/search' );
 const items = require( './controllers/items' );
@@ -8,5 +12,27 @@ router.get( '/search', search.search );
 router.get( '/items/:id', items.getItem );
 router.get( '/items/:id/pictures', items.getItemPictures );
 router.get( '/items/:id/description', items.getItemDescription );
+
+router.get( '/ip', ( req, res ) => {
+  try {
+    let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${ipApiKey}&ip=${req.ip}`;
+
+    request( url, { json: true } ).then( response => {
+      res.json( {
+        ip: response.ip,
+        zipCode: response.zipcode
+      } );
+    } ).catch( error => {
+      res.json( {
+        ip: req.ip,
+        zipCode: '98177'
+      } );
+    } );
+  } catch ( error ) {
+    console.error( error );
+    res.sendStatus( 500 );
+  }
+
+} );
 
 module.exports = router;
