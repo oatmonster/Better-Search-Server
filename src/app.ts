888bba65ab69bd553f068df744a2b73559345e59
Alex Zhao <oatmonster@gmail.com>
2019-12-01 18:41:05 -0800

    Begin typescript integration

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-const express = require( 'express' );
-const cors = require( 'cors' );
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 app.use( cors() );
 
-app.use( '/api', require( './api' ) );
+//app.use( '/api', require( './api' ) );
 
 app.get( '/', ( req, res ) => res.send( 'Copyright Alex Zhao 2019' ) );
 
