288489dc328b366ecc2f7f1f638e5d2f6acc8fc7
oatmonster <alexzhaowa@gmail.com>
2019-12-07 13:14:16 -0800

    Added custom error for failed Http requests

--- src/api/v2/controllers/search.ts
+++ src/api/v2/controllers/search.ts
@@ -1,6 +1,7 @@
 import request from 'request-promise-native';
 import xml2js from 'xml2js';
 import { IItem, ISearchResult } from '../common/interfaces';
+import { HttpError } from '../common/utils';
 
 const parser = new xml2js.Parser( { 'explicitArray': false } ).parseStringPromise;
 const appId = process.env.APP_ID;
@@ -8,6 +9,8 @@ const ipApiKey = process.env.IP_API_KEY;
 
 const search = ( req, res ) => {
   console.log( 'REQUEST Search query:', req.query );
+
+
 
   let searchUrl = 'https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.13.0&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD=true'
   searchUrl += '&SECURITY-APPNAME=' + appId;
@@ -17,103 +20,104 @@ const search = ( req, res ) => {
   let filterIndex = 0;
   let selectorIndex = 1;
 
-  // ====================================================================================
-  // Query
-  // ====================================================================================
-  if ( req.query.hasOwnProperty( 'query' ) ) {
-    searchUrl += '&keywords=' + req.query.query;
-  } else {
-    // Throw Error
-  }
-
-  // ====================================================================================
-  // Pagination
-  // ====================================================================================
-  if ( req.query.hasOwnProperty( 'page' ) ) {
-    if ( Number.isInteger( +req.query.page ) && +req.query.page > 1 ) {
-      searchUrl += '&paginationInput.pageNumber=' + req.query.page;
+  try {
+    // ====================================================================================
+    // Query
+    // ====================================================================================
+    if ( req.query.hasOwnProperty( 'query' ) ) {
+      searchUrl += '&keywords=' + req.query.query;
     } else {
-      // Throw Error
+      throw new HttpError( 'Keyword is required', 400 );
     }
-  }
 
-  // ====================================================================================
-  // Sorting
-  // ====================================================================================
-  const validSortings = new Map( [
-    [ 1, 'EndTimeSoonest' ],
-    [ 2, 'StartTimeNewest' ],
-    [ 3, 'PricePlusShippingLowest' ],
-    [ 4, 'PricePlusShippingHighest' ]
-  ] );
-
-  if ( req.query.hasOwnProperty( 'sortBy' ) ) {
-    if ( validSortings.has( +req.query.sortBy ) ) {
-      searchUrl += '&sortOrder=' + validSortings.get( +req.query.sortBy );
-    } else {
-      // Throw Error
+    // ====================================================================================
+    // Pagination
+    // ====================================================================================
+    if ( req.query.hasOwnProperty( 'page' ) ) {
+      if ( Number.isInteger( +req.query.page ) && +req.query.page > 1 ) {
+        searchUrl += '&paginationInput.pageNumber=' + req.query.page;
+      } else {
+        throw new HttpError( 'Invalid page', 400 );
+      }
     }
-  }
 
-  // ====================================================================================
-  // Category
-  // ====================================================================================
+    // ====================================================================================
+    // Sorting
+    // ====================================================================================
+    const validSortings = new Map( [
+      [ 1, 'EndTimeSoonest' ],
+      [ 2, 'StartTimeNewest' ],
+      [ 3, 'PricePlusShippingLowest' ],
+      [ 4, 'PricePlusShippingHighest' ]
+    ] );
 
-  if ( req.query.hasOwnProperty( 'category' ) ) {
-    searchUrl += '&categoryId=' + req.query.category;
-  }
-
-  // ====================================================================================
-  // Filtering -- Listing Type
-  // ====================================================================================
-
-  if ( req.query.hasOwnProperty( 'listType' ) ) {
-    if ( req.query.listType === '1' ) {
-      searchUrl += `&itemFilter(${filterIndex}).name=ListingType`;
-      searchUrl += `&itemFilter(${filterIndex}).value(0)=AuctionWithBIN`;
-      searchUrl += `&itemFilter(${filterIndex}).value(1)=FixedPrice`;
-      searchUrl += `&itemFilter(${filterIndex}).value(2)=StoreInventory`;
-      filterIndex++;
-    } else if ( req.query.listType === '2' ) {
-      searchUrl += `&itemFilter(${filterIndex}).name=BestOfferOnly`;
-      searchUrl += `&itemFilter(${filterIndex}).value(0)=true`;
-      filterIndex++;
-    } else if ( req.query.listType === '3' ) {
-      searchUrl += `&itemFilter(${filterIndex}).name=ListingType`;
-      searchUrl += `&itemFilter(${filterIndex}).value(0)=AuctionWithBIN`;
-      searchUrl += `&itemFilter(${filterIndex}).value(1)=Auction`;
-      filterIndex++;
-    } else {
-      // Throw Error
+    if ( req.query.hasOwnProperty( 'sortBy' ) ) {
+      if ( validSortings.has( +req.query.sortBy ) ) {
+        searchUrl += '&sortOrder=' + validSortings.get( +req.query.sortBy );
+      } else {
+        throw new HttpError( 'Invalid sorting', 400 );
+      }
     }
-  }
 
-  // ====================================================================================
-  // Filtering -- Condition
-  // ====================================================================================
+    // ====================================================================================
+    // Category
+    // ====================================================================================
 
-  if ( req.query.hasOwnProperty( 'condition' ) ) {
-    searchUrl += `&itemFilter(${filterIndex}).name=Condition`
-    searchUrl += `&itemFilter(${filterIndex}).value(0)=${req.query.condition}`
-  }
+    if ( req.query.hasOwnProperty( 'category' ) ) {
+      searchUrl += '&categoryId=' + req.query.category;
+    }
 
-  let zipCode = '';
-  let timeZone = '';
+    // ====================================================================================
+    // Filtering -- Listing Type
+    // ====================================================================================
 
-  request( `https://api.ipgeolocation.io/timezone?apiKey=${ipApiKey}&ip=${req.ip}` ).then( response => {
-    searchUrl += `&buyerPostalCode=${response.geo.zipcode}`;
-  } ).catch( error => {
-    searchUrl += `&buyerPostalCode=98177`;
-  } ).then( () => {
-    return request.get( searchUrl );
-  } ).then( response => {
-    return parser( response );
-  } ).then( result => {
-    result = result.findItemsAdvancedResponse;
-    if ( result.ack == 'Failure' ) {
-      // Process error message to get invalid parameter
-      res.status( 400 ).json( 'Failure ack' );
-    } else {
+    if ( req.query.hasOwnProperty( 'listType' ) ) {
+      if ( req.query.listType === '1' ) {
+        searchUrl += `&itemFilter(${filterIndex}).name=ListingType`;
+        searchUrl += `&itemFilter(${filterIndex}).value(0)=AuctionWithBIN`;
+        searchUrl += `&itemFilter(${filterIndex}).value(1)=FixedPrice`;
+        searchUrl += `&itemFilter(${filterIndex}).value(2)=StoreInventory`;
+        filterIndex++;
+      } else if ( req.query.listType === '2' ) {
+        searchUrl += `&itemFilter(${filterIndex}).name=BestOfferOnly`;
+        searchUrl += `&itemFilter(${filterIndex}).value(0)=true`;
+        filterIndex++;
+      } else if ( req.query.listType === '3' ) {
+        searchUrl += `&itemFilter(${filterIndex}).name=ListingType`;
+        searchUrl += `&itemFilter(${filterIndex}).value(0)=AuctionWithBIN`;
+        searchUrl += `&itemFilter(${filterIndex}).value(1)=Auction`;
+        filterIndex++;
+      } else {
+        throw new HttpError( 'Invalid list type', 400 );
+      }
+    }
+
+    // ====================================================================================
+    // Filtering -- Condition
+    // ====================================================================================
+
+    if ( req.query.hasOwnProperty( 'condition' ) ) {
+      searchUrl += `&itemFilter(${filterIndex}).name=Condition`
+      searchUrl += `&itemFilter(${filterIndex}).value(0)=${req.query.condition}`
+    }
+
+    let zipCode = '';
+    let timeZone = '';
+
+    request( `https://api.ipgeolocation.io/timezone?apiKey=${ipApiKey}&ip=${req.ip}` ).then( response => {
+      searchUrl += `&buyerPostalCode=${response.geo.zipcode}`;
+    } ).catch( error => {
+      searchUrl += `&buyerPostalCode=98177`;
+    } ).then( () => {
+      return request.get( searchUrl );
+    } ).then( response => {
+      return parser( response );
+    } ).then( result => {
+      result = result.findItemsAdvancedResponse;
+      if ( result.ack !== 'Success' ) {
+        // TODO: Check ebay error message to find cause of failure 
+        throw new HttpError( 'Failed to get item info', 400 );
+      }
       let clean: ISearchResult = {
         searchResult: {
           count: result.searchResult[ '$' ].count,
@@ -188,15 +192,25 @@ const search = ( req, res ) => {
       } );
 
       res.status( 200 ).json( clean );
+
+    } ).catch( error => {
+      if ( error instanceof HttpError ) {
+        console.error( error.toString() );
+        res.sendStatus( error.status );
+      } else {
+        console.error( error );
+        res.sendStatus( 500 );
+      }
+    } );
+  } catch ( error ) {
+    if ( error instanceof HttpError ) {
+      console.error( error.toString() );
+      res.sendStatus( error.status );
+    } else {
+      console.error( error );
+      res.sendStatus( 500 );
     }
-  } ).catch( error => {
-    // console.error( error.message );
-    if ( false /* Error was caused by bad parameter*/ ) {
-      res.sendStatus( 400 );
-    }
-    res.sendStatus( 500 );
-    console.error( error );
-  } );
+  }
 }
 
 export { search };
